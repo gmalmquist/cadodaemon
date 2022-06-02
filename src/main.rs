ce8688ba289c41a7edb7b1ff447a239242b17d13
Gwen Malmquist <gdmalmquist@gmail.com>
2022-06-01 20:18:14 -0400

    roundtripping from obj

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,14 @@ mod mesh;
 
 fn main() {
     println!("Hello, world!");
+
+    let obj = obj::Obj::load(std::path::Path::new("./test-meshes/cube.obj"))
+        .expect("Failed to load test input file ./test-meshes/cube.obj");
+    let mesh: mesh::Mesh = obj.into();
+    let obj: obj::Obj = mesh.into();
+
+    obj.save(std::path::Path::new("./test-meshes/output.obj"))
+        .expect("Failed to save output obj.");
+
+    println!("Done.");
 }
--- src/mesh.rs
+++ src/mesh.rs
@@ -1,5 +1,5 @@
-use nalgebra;
-use nalgebra::Vector3;
+use nalgebra::{Point, Point3, Vector3};
+use obj::Group;
 
 #[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash, Debug)]
 pub struct Face(usize);
@@ -13,36 +13,79 @@ pub struct Corner {
     pub index: usize,
 }
 
+#[derive(Clone, Debug)]
 pub struct Mesh {
-    pub vertices: Vec<Vector3<f64>>,
+    pub vertices: Vec<Point3<f64>>,
     pub faces: Vec<Vec<Vertex>>,
-    pub normals: Vec<Option<Vector3<f64>>>
+    pub normals: Vec<Option<Vector3<f64>>>,
 }
 
-pub struct FaceCornersIter {
-    face: Face,
+pub struct TopologyIter<T> {
     index: usize,
     count: usize,
+    func: Box<dyn Fn(usize) -> T>,
 }
 
-impl Iterator for FaceCornersIter {
-    type Item = Corner;
+impl<T> TopologyIter<T> {
+    pub fn new(count: usize, func: Box<dyn Fn(usize) -> T>) -> Self {
+        Self {
+            index: 0,
+            count,
+            func,
+        }
+    }
+}
+
+impl<T> Iterator for TopologyIter<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.index >= self.count {
             return None;
         }
-        let index = self.index;
+        let item = (self.func)(self.index);
         self.index += 1;
-        Some(Corner {
-            face: self.face,
-            index,
-        })
+        Some(item)
     }
 }
 
 impl Mesh {
-    fn compute_normals(&mut self) {
+    pub fn new() -> Self {
+        Self {
+            vertices: vec![],
+            faces: vec![],
+            normals: vec![],
+        }
+    }
+
+    pub fn add_vertex(&mut self, point: Point3<f64>, dedup_eps: Option<f64>) -> Vertex {
+        if let Some(eps) = dedup_eps {
+            for vi in 0..self.vertices.len() {
+                let v = &self.vertices[vi];
+                if nalgebra::distance(v, &point) < eps {
+                    return Vertex(vi);
+                }
+            }
+        }
+        self.vertices.push(point);
+        Vertex(self.vertices.len() - 1)
+    }
+
+    pub fn add_face(&mut self, vertices: Vec<Vertex>) -> Face {
+        self.faces.push(vertices);
+        let face = Face(self.faces.len() - 1);
+        face
+    }
+
+    pub fn faces(&self) -> TopologyIter<Face> {
+        TopologyIter::new(self.faces.len(), Box::new(|i| Face(i)))
+    }
+
+    pub fn vertices(&self) -> TopologyIter<Vertex> {
+        TopologyIter::new(self.vertices.len(), Box::new(|i| Vertex(i)))
+    }
+
+    pub fn compute_normals(&mut self) {
         self.assert_valid();
         self.normals.clear();
         for i in 0..self.faces.len() {
@@ -50,7 +93,7 @@ impl Mesh {
         }
     }
 
-    fn validate(&self) -> bool {
+    pub fn validate(&self) -> bool {
         for f in 0..self.faces.len() {
             if !Face(f).is_valid(self) {
                 return false;
@@ -59,13 +102,13 @@ impl Mesh {
         true
     }
 
-    fn assert_valid(&self) {
+    pub fn assert_valid(&self) {
         assert!(self.validate());
     }
 }
 
 impl Face {
-    pub fn to_vertices(self, mesh: &Mesh) -> &Vec<Vertex> {
+    pub fn vertices(self, mesh: &Mesh) -> &Vec<Vertex> {
         &mesh.faces[self.0]
     }
 
@@ -73,18 +116,25 @@ impl Face {
         mesh.faces[self.0].len()
     }
 
-    pub fn corners(&self, mesh: &Mesh) -> FaceCornersIter {
-        FaceCornersIter {
-            face: self.clone(),
-            index: 0,
-            count: self.corner_count(mesh),
-        }
+    pub fn corners(&self, mesh: &Mesh) -> TopologyIter<Corner> {
+        let face = self.clone();
+        TopologyIter::new(mesh.faces[self.0].len(), Box::new(move |i| Corner {
+            face,
+            index: i,
+        }))
     }
 
     pub fn first_corner(&self) -> Corner {
         Corner {
             face: self.clone(),
             index: 0,
+        }
+    }
+
+    pub fn get_normal<'m>(&self, mesh: &'m Mesh) -> Option<&'m Vector3<f64>> {
+        match mesh.normals.get(self.0) {
+            Some(n) => n.as_ref(),
+            None => None,
         }
     }
 
@@ -124,8 +174,8 @@ impl Face {
         if corners.len() < 3 {
             return true;
         }
-        for a in 0..(corners.len()-1) {
-            for b in (a+1)..corners.len() {
+        for a in 0..(corners.len() - 1) {
+            for b in (a + 1)..corners.len() {
                 if corners[a] == corners[b] {
                     return true;
                 }
@@ -136,7 +186,7 @@ impl Face {
 }
 
 impl Vertex {
-    fn to_point(self, mesh: &Mesh) -> &Vector3<f64> {
+    fn to_point(self, mesh: &Mesh) -> &Point3<f64> {
         &mesh.vertices[self.0]
     }
 
@@ -174,7 +224,71 @@ impl Corner {
         }
     }
 
-    fn to_point(self, mesh: &Mesh) -> &Vector3<f64> {
+    fn to_point(self, mesh: &Mesh) -> &Point3<f64> {
         self.to_vertex(mesh).to_point(mesh)
+    }
+}
+
+impl From<obj::ObjData> for Mesh {
+    fn from(obj: obj::ObjData) -> Self {
+        // NB: it might be more correct to load this into multiple meshes?
+        // This loses information about sub-objects, though maybe that's metadata that can just
+        // be added to the mesh later.
+        let mut mesh = Mesh::new();
+        for p in obj.position {
+            mesh.add_vertex(Point3::from_slice(&[p[0] as f64, p[1] as f64, p[2] as f64]), None);
+        }
+        for o in obj.objects {
+            for group in o.groups {
+                for poly in group.polys {
+                    let mut face = vec![];
+                    for obj::IndexTuple(pos, _, _) in poly.0 {
+                        face.push(Vertex(pos));
+                    }
+                    mesh.add_face(face);
+                }
+            }
+        }
+        mesh
+    }
+}
+
+impl From<obj::Obj> for Mesh {
+    fn from(obj: obj::Obj) -> Self {
+        obj.data.into()
+    }
+}
+
+impl From<Mesh> for obj::Obj {
+    fn from(mesh: Mesh) -> Self {
+        let mut data = obj::ObjData {
+            position: mesh.vertices.iter()
+                .map(|v| [v.x as f32, v.y as f32, v.z as f32])
+                .collect(),
+            objects: vec![
+                obj::Object {
+                    name: "mesh".to_string(),
+                    groups: vec![obj::Group {
+                        name: "mesh".to_string(),
+                        index: 0,
+                        material: None,
+                        polys: mesh.faces()
+                            .map(|face| obj::SimplePolygon(
+                                face.vertices(&mesh).iter()
+                                    .map(|v| obj::IndexTuple(v.0, None, None))
+                                    .collect()
+                            ))
+                            .collect()
+                    }]
+                }
+            ],
+            material_libs: vec![],
+            normal: vec![],
+            texture: vec![],
+        };
+        obj::Obj {
+            data,
+            path: std::path::PathBuf::new(),
+        }
     }
 }
